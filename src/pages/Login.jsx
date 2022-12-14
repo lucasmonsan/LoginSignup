07632d05b3bc51ef1afef7c0b9d57adfcf82402c
lucasmonsan <lucasmonsan@gmail.com>
2022-12-14 18:02:10 -0300

    Correções de verificação de senha e exclusão do useForm.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,7 +21,7 @@ export const Login = () => {
     
     setTimeout(() => {
       setLoading(false);
-      if (email === "positive@email.com") {
+      if (email === "positive@email.com" && password === "8415627390") {
         localStorage.setItem("labexTkn", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuYXV0aDAuY29tLyIsImF1ZCI6Imh0dHBzOi8vYXBpLmV4YW1wbGUuY29tL2NhbGFuZGFyL3YxLyIsInN1YiI6InVzcl8xMjMiLCJpYXQiOjE0NTg3ODU3OTYsImV4cCI6MTQ1ODg3MjE5Nn0.CA7eaHjIHz5NxeIJoFK9krqaeZrPLwmMmgI_XiQiIkQ");
         setModal({trigger: true, type: "success", title: "Login autorizado!", text: "Entrando no foguete..."})        
         setTimeout(() => goToHome(),1500);
